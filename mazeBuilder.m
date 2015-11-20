de17dbdb91c4797e95e0fd653379fcdce2180a5f
David Medine <dmedine@ucsd.edu>
2015-11-20 12:47:53 -0800

    created and tested mazeBuilder

--- mazeBuilder.m
+++ mazeBuilder.m
@@ -1,38 +1,48 @@
 % function to create custom mazes for the audio maze
-% the outer walls (and the northwest and southeast corners) are
-% predetermined
-% the rest of the maze information is supplied as a maze_representation
-% argumen -- this is an array of 4xm*n elements, one for each panel of the
-% maze
-% the first column is the north panel (0 for open, 1 for closed)
-% second for east, third for south, fourth for west
-%
-% for example, the panel given here (plusses for openenings, bars for walls):
-%          N
-%    | + + + + +
-%    |         + 
-%  W |         + E
-%    |         +
-%    | - - - - +
-%          S
-% would be shown as the row [0 0 1 1] in this representation
 % 
+% It turns out that one can entirely describe a gridded maze provided one
+% knows the outer walls and whether or not one can proceed either to the
+% right or downward starting in the top left.
 %
-% The order of tiles is right to left, row to row, starting from the 
-% bottom right. That is to say a 3x3 maze would have panels in the  
+% The order of tiles is left to right, row to row, starting from the 
+% top left. That is to say a 3x3 maze would have panels in the  
 % following order:
 %
 %  ------------- 
-%  | 9 | 8 | 7 |
+%  | 1 | 2 | 3 |
 %  -------------
-%  | 6 | 5 | 4 |
+%  | 4 | 5 | 5 |
 %  -------------
-%  | 3 | 2 | 1 |
+%  | 7 | 8 | 9 |
 %  -------------
 %
 %
-% As mentioned, the NorthWest (exit) and SouthEast (entrance) corners are
-% predetermined, as are all the walls
+% The NorthWest (exit) and SouthEast (entrance) corners are
+% predetermined, as are all the outer walls
+%
+% In order to specify the maze, for eac panel give a 0 (open) or 1 (closed)
+% for the right and bottom passages respectively. For example the following
+% maze (plusses are for openings, bars for walls):
+%
+%  -+++--------- 
+%  | 1 | 2 + 3 |
+%  -+++-----+++-
+%  | 4 X 5 | 5 |
+%  -+++-----+++-
+%  | 7 X 8 X 9 |
+%  ---------+++-
+%
+% Would be written:
+% maze = [1, 0;  ...% 1
+%         0, 1;  ...% 2
+%         1, 0;  ...% 3
+%         0, 0;  ...% 4
+%         1, 1;  ...% 5
+%         1, 0;  ...% 6
+%         0, 1;  ...% 7
+%         0, 1;  ...% 8
+%         1, 0;];   % 9
+%
 %
 % the return variable is an array of points that adhere to the coordinate
 % system given by the random maze generator make_maaze_polygons
@@ -44,10 +54,17 @@
 % Notes:
 % The random maze builder delivers a maze that gets rotated in the
 % audioMaze process. This means that the meaning of 'rows' and 'columns'
-% gets flipped. The user of this function should design the maze in the
+% gets reversed. The user of this function should design the maze in the
 % coordinate system and orientation of the mobilab, the code will do the
-% rotations to make it conform to the rest of the code in the maze.
+% rotation to make it conform to the rest of the code in the maze. But, if
+% you inspect the maze using audioMaze.plotMaze, its orientation is flipped
+% on both the y and x axes. This is actually how the maze is represented in
+% adherence to the mocap orientation. It is flipped graphically when the
+% maze is running to make it more human-readable.
 % 
+% The outer walls are always the same, with the exit at the top of the
+% left most panel and the entrance at the bottom of the righ most. The
+% inner walls are specified. The algorithm only draws the 
 % 
 % 
 % Arguments:
@@ -73,11 +90,6 @@ function maze_lines = mazeBuilder(rows, cols, maze_representation)
         return;
     end
     
-    % check that we have an entrance and exit
-    if maze_representation(1,1) ~= 0 | maze_representation(rows*cols, 3) ~= 0
-        display('NorthWest (first top) and SouthEast (last bottom) horizontal segments must be empty');
-        return;
-    end
     
     n = cols;
     m = rows; 
@@ -90,6 +102,7 @@ function maze_lines = mazeBuilder(rows, cols, maze_representation)
     maze_lines(3,:) = [.5, .5, 1.5, n+.5];
     maze_lines(4,:) = [m+.5, m+.5, .5, n-.5];
     
+
     figure;
     hold on;
     for i= 1:4
@@ -97,24 +110,24 @@ function maze_lines = mazeBuilder(rows, cols, maze_representation)
     end
     panel_cnt = 1;
     ln_cnt = 5;
-    for i = 0:m-1
-        for k = 0:n-1
+    for i = 1:m
+        for k = 1:n
             
-            for l = 1:4
-                if l == 1 % different for each side
-                    if maze_representation(panel_cnt, l) == 1 % draw a line here
-                        maze_lines(ln_cnt, 1) = i - .5;
-                        maze_lines(ln_cnt, 2) = i - .5;
-                        maze_lines(ln_cnt, 3) = k - .5;
-                        maze_lines(ln_cnt, 4) = k + .5;
-
-                        line(maze_lines(ln_cnt, 1:2), maze_lines(ln_cnt, 3:4), 'linewidth', 3);
-
-                        ln_cnt = ln_cnt + 1;
-                    end
-                end
+            for l = 1:2 % only draw the right and bottom segments
+%                 if l == 1 & i~=1 % skip the top on the top row
+%                     if maze_representation(panel_cnt, l) == 1 % draw a line here
+%                         maze_lines(ln_cnt, 1) = i - .5;
+%                         maze_lines(ln_cnt, 2) = i - .5;
+%                         maze_lines(ln_cnt, 3) = k - .5;
+%                         maze_lines(ln_cnt, 4) = k + .5;
+% 
+%                         line(maze_lines(ln_cnt, 1:2), maze_lines(ln_cnt, 3:4), 'linewidth', 3);
+% 
+%                         ln_cnt = ln_cnt + 1;
+%                     end
+%                 end
                 
-                if l == 2 % different for each side
+                if l == 1 & k~=n % skip the right on the last column 
                     if maze_representation(panel_cnt, l) == 1 % draw a line here
                         maze_lines(ln_cnt, 1) = i - .5;
                         maze_lines(ln_cnt, 2) = i + .5;
@@ -126,7 +139,7 @@ function maze_lines = mazeBuilder(rows, cols, maze_representation)
                     end
                 end
                 
-                if l == 3 % different for each side
+                if l == 2 & i~=m % skip the bottom on the bottom row 
                     if maze_representation(panel_cnt, l) == 1 % draw a line here
                         maze_lines(ln_cnt, 1) = i + .5;
                         maze_lines(ln_cnt, 2) = i + .5;
@@ -138,22 +151,26 @@ function maze_lines = mazeBuilder(rows, cols, maze_representation)
                     end
                 end
                 
-                if l == 4 % different for each side
-                    if maze_representation(panel_cnt, l) == 1 % draw a line here
-                        maze_lines(ln_cnt, 1) = i - .5;
-                        maze_lines(ln_cnt, 2) = i + .5;
-                        maze_lines(ln_cnt, 3) = k - .5;
-                        maze_lines(ln_cnt, 4) = k - .5;
-
-                        line(maze_lines(ln_cnt, 1:2), maze_lines(ln_cnt, 3:4), 'linewidth', 3);
-                        ln_cnt = ln_cnt + 1;
-                    end
-                end
+%                 if l == 4 & i~=1 % skip the left 
+%                     if maze_representation(panel_cnt, l) == 1 % draw a line here
+%                         maze_lines(ln_cnt, 1) = i - .5;
+%                         maze_lines(ln_cnt, 2) = i + .5;
+%                         maze_lines(ln_cnt, 3) = k - .5;
+%                         maze_lines(ln_cnt, 4) = k - .5;
+% 
+%                         line(maze_lines(ln_cnt, 1:2), maze_lines(ln_cnt, 3:4), 'linewidth', 3);
+%                         ln_cnt = ln_cnt + 1;
+%                     end
+%                 end
                 
             end
             panel_cnt = panel_cnt + 1;
         end
     end
     hold off;
+    
+
+
+    
     
 end
