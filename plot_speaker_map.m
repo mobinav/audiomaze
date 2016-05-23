9023c7144bad1f4b53249f7ea66a849b3e570b38
David Medine <dmedine@ucsd.edu>
2016-05-23 16:21:53 -0700

    improved speaker plotter

--- plot_speaker_map.m
+++ plot_speaker_map.m
@@ -17,18 +17,21 @@ for n=1:12
         'markeredge', [.3 .5 .7], ...
         'markerface', [.3 .7 .7], ...
         'linewidth', 2);
-    text(speaker_locs(n,1)-.4, speaker_locs(n,2)-.2, sprintf('%d',n), 'fontsize', 14);
+    text(speaker_locs(n,1)-.1, speaker_locs(n,2)-.5, sprintf('%d',n), 'fontsize', 14);
 
 end
 
 for n=13:20
-    scatter(speaker_locs(n,1),speaker_locs(n,2),100, ... 
+    scatter(speaker_locs(n,1),speaker_locs(n,2),200, 'x', ... 
         'markeredge', [.7 .5 .3], ...
         'markerface', [.7 .7 .3], ...
         'linewidth', 2);
-    text(speaker_locs(n,1)-.4, speaker_locs(n,2)-.2, sprintf('%d',n), 'fontsize', 14);
+    text(speaker_locs(n,1)+.2, speaker_locs(n,2)-.3, sprintf('%d',n), 'fontsize', 14);
 end
 text(-w/2,-h/2+.5 , 'door', 'fontsize', 14);
+title('Audiomaze Speaker Map', 'fontsize', 14, 'fontweight', 'bold')
+set(gca, 'fontsize', 14); 
 %set(gca, 'XDir', 'reverse');
 set(gca, 'YDir', 'reverse');
+axis equal
 hold off
