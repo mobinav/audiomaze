ef533885f97e4d7f4f497711928f756ae3b76bb5
John R Iversen <john@johniversen.org>
2017-03-23 18:14:19 -0700

    Begin to check make_maze_polygons

--- make_maze_polygons_nr.m
+++ make_maze_polygons_nr.m
@@ -1,79 +1,64 @@
+function [mazePolygons, mazeLines]= make_maze_polygons_nr(row,col, wallThickness, ptr_right, ptr_down, exits)
+% make_maze_polygons_nr  generate geometry for 'nonrandom' mazes
+%
+%   [mazePolygons mazeLines]= make_maze_polygons_nr(row,col, wallThickness, ptr_right, ptr_down, exits)
+%
 % Adapted from Rodney Meyer's file, make_maze_polygons.m. Adaptation for
 % AudioMaze at SCCN, UCSD by David Medine.
 
 % Everything is the same, but this code does not use a random seed. Instead
 % information as to whether or not a current maze square opens to the right
 % or down is supplied. This will completely characterize the maze.
-
-function [mazePolygons mazeLines]= make_maze_polygons_nr(row,col, wallThickness, ptr_right, ptr_down)
-% usage  thyme = maze(30,45,'c');
+%
 % row - number of rows in the maze
 % col - number of column in the maze
-% wallThickness - thickness of the wall
-% ptr_right - rowxcol array of right passages
-% ptr_down - rowxcol array of down passages
+% wallThickness - thickness of the wall (normalized)
+% ptr_right - rowxcol array of right walls
+% ptr_down - rowxcol array of down walls
+% exits - true, make exits in NW and SE corners of maze (default false)
+%
+% mazePolygons n x 2, coordinates of polygon defining maze (useful forplotting)
+% mazeLines    n x 4, line segments (x1 x2 y1 y2) of lines defining walls
+%                       consists of 4 outer walls and inner walls
+
+if nargin < 6
+    exits = false;
+end
 
 [cc,rr]=meshgrid(1:col,1:row);
-state = reshape([1:row*col],row,col); % state identifies connected regions
-id = reshape([1:row*col],row,col); % id identifies intersections of maze
-
-% create pointers to adjacent intersections
-ptr_left = zeros(size(id));
-ptr_up = zeros(size(id));
-
-ptr_left(:,2:size(id,2)) = id(:,1:size(id,2)-1);
-ptr_up(2:size(id,1),:) = id(1:size(id,1)-1,:);
-
-% sort graph entities by id
-the_maze = cat(2,reshape(id,row*col,1),reshape(rr,row*col,1),reshape(cc,row*col,1),reshape(state,row*col,1),...
-    reshape(ptr_left,row*col,1),reshape(ptr_up,row*col,1),reshape(ptr_right,row*col,1),reshape(ptr_down,row*col,1)  );
-
-the_maze = sortrows(the_maze);
-
-id=the_maze(:,1);
-rr=the_maze(:,2);
-cc=the_maze(:,3);
-state=the_maze(:,4);
-
-clear the_maze;
-
-% create a random maze
-
-% show maze
-%h=figure('color','white');
+% NB (JRI 3/17) Must transpose because our maze definition is row-major (count along
+% columns) while matlab is column-major! Remember c=x, r=y
+rr=rr'; cc=cc';
 mazePolygons = []; mazeLines = [];
-h = 0;
-[mazePolygons mazeLines] = show_maze(mazePolygons, mazeLines, wallThickness, row, col, rr, cc, ptr_right, ptr_down);
+[mazePolygons, mazeLines] = show_maze(mazePolygons, mazeLines, wallThickness, row, col, rr, cc, ptr_right, ptr_down, exits);
 
 return
 
-function move_spot(src,evnt)
-assignin('caller','key',evnt.Key)
-return
 
+function [mazePolygons, mazeLines] = show_maze(mazePolygons, mazeLines, wallThickness,  row, col, rr, cc, ptr_right, ptr_down, exits)
 
-function [mazePolygons mazeLines] = show_maze(mazePolygons, mazeLines, wallThickness,  row, col, rr, cc, ptr_right, ptr_down)
-%figure(h)
-%hold on;
-% figure;
-% hold on;
-% axis([.5,col+.5,.5,row+.5])
-
-[mazePolygons mazeLines] = line_and_polygon([.5,col+.5],[.5,.5], mazePolygons,mazeLines, wallThickness); % draw top border
-[mazePolygons mazeLines] = line_and_polygon([.5,col+.5],[row+.5,row+.5], mazePolygons,mazeLines, wallThickness); % draw bottom border
-[mazePolygons mazeLines] = line_and_polygon([.5,.5],[1.5,row+.5], mazePolygons,mazeLines, wallThickness); % draw left border
-[mazePolygons mazeLines] = line_and_polygon([col+.5,col+.5],[.5,row-.5], mazePolygons,mazeLines, wallThickness);  % draw right border
-
+%define outer walls
+if exits
+    [mazePolygons, mazeLines] = line_and_polygon([.5,col-.5],[.5,.5], mazePolygons,mazeLines, wallThickness); % draw top border (with opening)
+    [mazePolygons,  mazeLines] = line_and_polygon([1.5,col+.5],[row+.5,row+.5], mazePolygons,mazeLines, wallThickness); % draw bottom border (with opening)
+    [mazePolygons,  mazeLines] = line_and_polygon([.5,.5],[.5,row+.5], mazePolygons,mazeLines, wallThickness); % draw left border
+    [mazePolygons,  mazeLines] = line_and_polygon([col+.5,col+.5],[.5,row+.5], mazePolygons,mazeLines, wallThickness);  % draw right border
+else
+    [mazePolygons,  mazeLines] = line_and_polygon([.5,col+.5],[.5,.5], mazePolygons,mazeLines, wallThickness); % draw top border (no opening)
+    [mazePolygons,  mazeLines] = line_and_polygon([.5,col+.5],[row+.5,row+.5], mazePolygons,mazeLines, wallThickness); % draw bottom border (no opening)
+    [mazePolygons,  mazeLines] = line_and_polygon([.5,.5],[.5,row+.5], mazePolygons,mazeLines, wallThickness); % draw left border
+    [mazePolygons,  mazeLines] = line_and_polygon([col+.5,col+.5],[.5,row+.5], mazePolygons,mazeLines, wallThickness);  % draw right border
+end
 
 
 for ii=1:length(ptr_right)
     if ptr_right(ii)>0 % right passage blocked
-        [mazePolygons mazeLines] = line_and_polygon([cc(ii)+.5,cc(ii)+.5],[rr(ii)-.5,rr(ii)+.5], mazePolygons, mazeLines,wallThickness);
+        [mazePolygons,  mazeLines] = line_and_polygon([cc(ii)+.5,cc(ii)+.5],[rr(ii)-.5,rr(ii)+.5], mazePolygons, mazeLines,wallThickness);
         %hold on
         
     end
     if ptr_down(ii)>0 % down passage blocked
-        [mazePolygons mazeLines] = line_and_polygon([cc(ii)-.5,cc(ii)+.5],[rr(ii)+.5,rr(ii)+.5], mazePolygons, mazeLines,wallThickness);
+        [mazePolygons,  mazeLines] = line_and_polygon([cc(ii)-.5,cc(ii)+.5],[rr(ii)+.5,rr(ii)+.5], mazePolygons, mazeLines,wallThickness);
         %hold on
     end
 
@@ -89,34 +74,29 @@ end
 % axis off
 % set(gca,'YDir','reverse')
 % hold off;
-return
 
 
-function [mazePolygons mazeLines] = line_and_polygon(x,y, mazePolygons,mazeLines, wallThickness)
+function [mazePolygons,  mazeLines] = line_and_polygon(x,y, mazePolygons,mazeLines, wallThickness)
 %line(x,y);
 
 for id = 1:(length(x)-1)
-    % make a perpendicular vector
+    % make direction and perpendicular vectors, to enable thickness
     dx = x(id+1) - x(id);
     dy = y(id+1) - y(id);
     
-   
     % extend both sides by width/2, to make polygons connect
     directionVector = [dx dy];
     directionVector = directionVector / norm(directionVector, 'fro');
     
     extensionRatio = 1;
-    x(id) = x(id) - wallThickness * extensionRatio * directionVector(1);
-    x(id+1) = x(id+1) + wallThickness * extensionRatio * directionVector(1);
+    x(id) = x(id) - wallThickness/2 * extensionRatio * directionVector(1);
+    x(id+1) = x(id+1) + wallThickness/2 * extensionRatio * directionVector(1);
     
-    y(id) = y(id) - wallThickness * extensionRatio * directionVector(2);
-    y(id+1) = y(id+1) + wallThickness * extensionRatio * directionVector(2);
+    y(id) = y(id) - wallThickness/2 * extensionRatio * directionVector(2);
+    y(id+1) = y(id+1) + wallThickness/2 * extensionRatio * directionVector(2);
     
     perpendicularVector = [-dy dx];
-    
-    
-    
-    perpendicularVector = wallThickness * perpendicularVector / norm(perpendicularVector, 'fro');
+    perpendicularVector = wallThickness/2 * perpendicularVector / norm(perpendicularVector, 'fro');
     
     polygonX = [x(id)-perpendicularVector(1) x(id+1)-perpendicularVector(1) x(id+1)+perpendicularVector(1) x(id)+perpendicularVector(1)];
     polygonY = [y(id)-perpendicularVector(2) y(id+1)-perpendicularVector(2) y(id+1)+perpendicularVector(2) y(id)+perpendicularVector(2)];
@@ -125,13 +105,16 @@ for id = 1:(length(x)-1)
     polygonX = [polygonX polygonX(1)];
     polygonY = [polygonY polygonY(1)];
     
+    %make clockwise
+    [polygonX, polygonY] = poly2cw(polygonX, polygonY);
+    
     % plot(polygonX, polygonY,'r');
    
     if isempty(mazePolygons)
        mazePolygons(1,:) = polygonX;
        mazePolygons(2,:) = polygonY;
     else
-        [mazePolygonXs mazePolygonYs] = polybool('union', mazePolygons(1,:),  mazePolygons(2,:), polygonX, polygonY);
+        [mazePolygonXs,  mazePolygonYs] = polybool('union', mazePolygons(1,:),  mazePolygons(2,:), polygonX, polygonY);
         mazePolygons = [mazePolygonXs; mazePolygonYs];
     end;
     
@@ -139,7 +122,6 @@ for id = 1:(length(x)-1)
         mazeLines = [x y];
     else
         mazeLines = [mazeLines; x y];
-    end;    
-end;
-return
+    end  
+end
 
