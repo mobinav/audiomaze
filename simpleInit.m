ac26bacbaaea5e3d027e19682426155376707cec
SCCN MoBI <mobi@presenting>
2017-04-02 19:03:06 -0700

    cleaning out unused code

--- simpleInit.m
+++ simpleInit.m
@@ -5,7 +5,7 @@ X = makeSimpleMaze(X);
 
 %% this function does the following
 % 1. initializes all the variables in the structure
-% 2. makes the maze by calling the audioMaze constructor (audioMaze.m)
+% 2. makes the maze by calling the audioMaze constructor (audioMaze.m) & plots maze
 % 3. concocts the VR representation if the last argument is 'true'
 % 4. sets up all the LSL outlets
 % 5. sets up the LSL inlet (looks for pahsespace stream) and defines the
@@ -21,44 +21,8 @@ X.tokens = mazeTokens(X.am, X.mazeGeometry.token_locs, []);
 X.tokenReached = zeros(1, length(X.mazeGeometry.token_locs));
 X.tokenReached(1) = 1; % ensure that the starting point is 'reached'
 
-%% plot maze
-%X.am.plot
-
-% plot them on the maze
+% plot tokens on the maze
 %start blue
 plot(X.tokens.mocapLocs(1,1), X.tokens.mocapLocs(1,2), 'o', 'color', [.5 .5 .9], 'markersize',30, 'linewidth', 3);
 %goal(s) red
 plot(X.tokens.mocapLocs(2:end,1), X.tokens.mocapLocs(2:end,2), 'o', 'color', [.9 .5 .5], 'markersize',30, 'linewidth', 3);
-
-%% stop the maze
-% X=stop_maze(X);
-% 
-% 
-% %% close the velocity file and read it
-% X=stop_maze(X);
-% if ~isempty(X.velocityFile)
-%     fclose(X.velocityFile);
-% end
-% fId = fopen('velocityFile', 'r');
-% formatSpec = '%f';
-% vel = fscanf(fId,formatSpec);
-% meanVel = mean(vel)
-% 
-% 
-% % emit stats here
-% statData = [meanVel, X.nearWallCntHand, X.touchingWallCntHand, X.totalTimeNearWallHand, X.totalTimeTouchingWallHand, X.nearWallCntHead, X.touchingWallCntHead, X.totalTimeNearWallHead, X.totalTimeTouchingWallHead];
-% X.LSL.emitStats(statData);
-% 
-% %% stop and clear the maze
-% X=stop_maze(X);
-% if ~isempty(X.velocityFile)
-%     fclose(X.velocityFile);
-% end
-% pause(2);
-% close(X.am.fig_handle);
-% clear all;
-% 
-% 
-% %% run once (for debugging)
-% simpleTaskCb;
-% 
